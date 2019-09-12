b9823ba94f31efec20e7d9146b78eb50031861d8
yanzheh <yanzheh@student.unimelb.edu.au>
2019-09-12 17:05:43 +1000

    DBConnection

--- src/com/shopping_mall/common/DBConnection.java
+++ src/com/shopping_mall/common/DBConnection.java
@@ -1,5 +1,42 @@
 package com.shopping_mall.common;
+import java.sql.*;
 
 public class DBConnection {
+    // JDBC driver name and database URL
+    private static final String DB_CONNECTION = "jdbc:postgresql://localhost:5432/shop";
 
+    // Database credentials
+    private static final String DB_USER = "postgres";
+    private static final String DB_PASSWORD = "******";
+
+    static Connection dbConnection = null;
+
+    public static PreparedStatement prepare(String stm) throws SQLException{
+        PreparedStatement preparedStatement = null;
+        try{
+            Connection dbConnection = getDBConnection();
+
+            preparedStatement = dbConnection.prepareStatement(stm);
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return preparedStatement;
+    }
+
+    private static Connection getDBConnection(){
+        try{
+            // Register JDBC driver
+            DriverManager.registerDriver(new org.postgresql.Driver());
+
+            //Open a connection
+            dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
+            dbConnection.setAutoCommit(false);
+
+            return dbConnection;
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Connection problem");
+        return null;
+    }
 }
